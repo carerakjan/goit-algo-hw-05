76dbad6a9d4b05dd3219aeb1f17be01371ebf6ec
Bo_Hr <carerakjan@gmail.com>
2024-03-12 13:30:33 +0200

    ex_2 added

---
+++ exercise_2/main.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+from typing import Generator, Any
+
+
+
+def generator_numbers(text: str):
+    for word in text.split():
+        if word.replace('.','', 1).isdigit():
+            yield Decimal(word).quantize(Decimal('0.00'))
+
+
+
+def sum_profit(txt: str, gen: Generator[Decimal, Any, None]):
+    return sum(gen(txt))
+
+
+
+def main():
+    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
+
+
+
+if __name__ == '__main__':
+    main()
